c2707f52e6f6f8ed5c58878c6f90593dda0982fa
James Fogarty <jayfo@users.noreply.github.com>
2025-09-14 08:14:43 -0700

    Initial Review of First Page

--- src/data/CourseStoreData.ts
+++ src/data/CourseStoreData.ts
@@ -2,9 +2,9 @@ import { CourseStoreData } from "@/types/CourseStore";
 
 export const courseStoreData: CourseStoreData = {
   // Link to course Canvas. No trailing slash.
-  linkCanvas: "https://canvas.uw.edu/courses/1746586",
+  linkCanvas: "https://canvas.uw.edu/courses/1828371",
   // Link to course GitHub.
-  linkGitHub: "https://github.com/uwcse440/web-cse440-24au",
+  linkGitHub: "https://github.com/uwcse440/web-cse440-25au",
   // Link to university syllabus policies and guidelines.
   linkUniversitySyllabusGuidelines:
     "https://registrar.washington.edu/curriculum/syllabus-guidelines/",
